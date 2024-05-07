3b9275c3ac274e74b4dc26d08f5404b39b68243a
doandung <doantrungdung2001@gmail.com>
2024-05-07 20:18:37 +0700

    update project app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
-
+require('dotenv').config()
+const compression = require('compression')
 const express = require('express')
 const { default: helmet } = require('helmet')
+const morgan = require('morgan')
+const cors = require('cors')
+const deleteUploads = require('./cron-tasks/delete-uploads')
 const app = express()
+
+// init moddlewares
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression())
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(cors())
+deleteUploads.start()
+
+// init db
+require('./dbs/init.mongodb')
+
+// checkOverload()
+// const { checkOverload } = require("./helpers/check.connect")
+
+// init routes
+app.use('/', require('./routes/'))
 
 // handling error
 app.use((req, res, next) => {
